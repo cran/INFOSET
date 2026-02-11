5055f4d5319ee10ad64f4b57aa859dde09c2bc58
Gloria Polinesi <g.polinesi@staff.univpm.it>
2026-02-11 15:50:02 +0000

    version 4.1.1

---
+++ R/utils_imports.R
@@ -0,0 +1,8 @@
+#' @importFrom graphics abline boxplot hist lines par points
+#' @importFrom stats cov density dlnorm median plnorm quantile sd
+#' @importFrom mixtools normalmixEM
+#' @importFrom quadprog solve.QP
+#' @importFrom Matrix nearPD
+#' @importFrom dendextend color_branches assign_values_to_leaves_nodePar
+#' @importFrom colorspace rainbow_hcl
+NULL
